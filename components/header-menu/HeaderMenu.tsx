13f6c29a40190eeb3b0c8e153996c7fe474a9e83
DanielFalzon <danie.falzon6@gmail.com>
2024-06-01 16:46:33 +0200

    Added data items for menu

--- components/header-menu/HeaderMenu.tsx
+++ components/header-menu/HeaderMenu.tsx
@@ -1,17 +1,22 @@
 // Implementation of https://mui.com/material-ui/react-app-bar/#responsive-app-bar-with-drawer
 'use client'
 
-import { AppBar, Box, Toolbar, IconButton } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography, IconButton, Button } from "@mui/material";
 import { Menu } from '@mui/icons-material';
-import { useState } from "react";
+import { FC, useState } from "react";
+import { MenuItem } from "@/types/menuItem";
 
-const HeaderMenu = () => {
+type HeaderMenuProps = {
+    menuItems: MenuItem[]
+}
+
+const HeaderMenu: FC<HeaderMenuProps> = ({menuItems}) => {
     const [mobileOpen, setMobileOpen] = useState(false);
 
     const handleDrawerToggle = () => {
         setMobileOpen((prevState) => !prevState);
     }
-
+    
     return (
         <Box sx={{ display: 'flex'}}>
             <AppBar component="nav">
@@ -25,6 +30,20 @@ const HeaderMenu = () => {
                     >
                         <Menu />
                     </IconButton>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+                    >
+                        Dan
+                    </Typography>
+                    <Box sx={{ display: { xs: 'none', sm: 'block'}}}>
+                        {menuItems.map((item) => (
+                            <Button key={item.name} sx={{ color: '#fff'}}>
+                                {item.name}
+                            </Button>
+                        ))}
+                    </Box>
                 </Toolbar>
             </AppBar>
         </Box>
