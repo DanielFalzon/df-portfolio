bb35dffd88030f30115a1dcc1dadfe459cb2fc63
DanielFalzon <danie.falzon6@gmail.com>
2024-06-07 12:19:41 +0200

    Initial working desktop experience section

--- components/experience-list-items-desktop/ExperienceListItemsDesktop.tsx
+++ components/experience-list-items-desktop/ExperienceListItemsDesktop.tsx
@@ -1,9 +1,15 @@
 import { ExperienceItem } from "@/types/Types";
-import { Tab, Tabs } from "@mui/material";
-import { FC, SyntheticEvent, useState } from "react";
+import { Box, Tab, Tabs, Typography } from "@mui/material";
+import { FC, ReactNode, SyntheticEvent, useState } from "react";
 
 type ExperienceListItemsDesktopProps = {
     experienceItems: ExperienceItem[]
+}
+
+type TabPanelProps = {
+    children?: ReactNode,
+    index: number,
+    value: number
 }
 
 const TabItemProps = (index: number) => {
@@ -14,6 +20,21 @@ const TabItemProps = (index: number) => {
     }
 }
 
+const TabPanel:FC<TabPanelProps> = ({children, index, value}) => (
+    <div 
+        role="tabpanel"
+        hidden={value !== index}
+        id={`vertical-tabpanel-${index}`}
+        aria-labelledby={`vertical-tab-${index}`}
+    >
+        {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+)
+
 const ExperienceListItemsDesktop:FC<ExperienceListItemsDesktopProps> = ({experienceItems}) => {
     const[value, setValue] = useState(0);
     
@@ -22,19 +43,30 @@ const ExperienceListItemsDesktop:FC<ExperienceListItemsDesktopProps> = ({experie
     }
 
     return(
-        <>
+        <Box
+            sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex' }}
+        >
             <Tabs
                 orientation="vertical"
                 value={value}
                 onChange={handleChange}
                 aria-label="Experience vertical tabs"
+                variant="standard"
+                sx={{
+                    width: '700px'
+                }}
             >
                 {experienceItems.map((item, index) => (
                     <Tab label={item.company} {...TabItemProps(index)} />
                 ))}
-
             </Tabs>
-        </>
+            {experienceItems.map((item, index) => (
+                <TabPanel key={index} value={value} index={index}>
+                    {item.description}
+                </TabPanel>
+            ))}
+
+        </Box>
     )
 }
 
